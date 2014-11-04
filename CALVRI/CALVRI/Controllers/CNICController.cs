427e9975c9236a76b7b334a33761c7d04404ec0b
ProjectCobra <sumbulaftab@gmail.com>
2014-11-04 15:49:15 +0500

    Master Project
    Initial project, only login pages and homepages complete.
    Controllers and models have not been setup yet

---
+++ CALVRI/CALVRI/Controllers/CNICController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CALVRI.Controllers
+{
+    public class CNICController : Controller
+    {
+        // GET: CNIC Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
