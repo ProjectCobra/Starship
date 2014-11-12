1b153ae781a0a313242beccc92d97bb32670e170
ProjectCobra <sumbulaftab@gmail.com>
2014-11-12 21:36:26 +0500

    Controllers and Models added

--- CALVRI/CALVRI/Controllers/CNICController.cs
+++ CALVRI/CALVRI/Controllers/CNICController.cs
@@ -14,6 +14,43 @@ namespace CALVRI.Controllers
             return View();
         }
 
+        //POST: Paychallan
+        [HttpPost]
+        public ActionResult Paychallan()
+        {
+            return View();
+        }
 
+        //POST: Pay Road Tax
+        [HttpPost]
+        public ActionResult PayRT()
+        {
+            return View();
+        }
+
+        //POST: Paychallan
+        [HttpPost]
+        public ActionResult LicenseRenew()
+        {
+            return View();
+        }
+
+        //GET: License Details
+        public ActionResult License()
+        {
+            return View();
+        }
+
+        //GET: Challan Details
+        public ActionResult Challan()
+        {
+            return View();
+        }
+
+        //GET: RoadTax Details
+        public ActionResult RoadTax()
+        {
+            return View();
+        }
     }
 }
---
+++ CALVRI/CALVRI/Models/Challan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CALVRI.Models
+{
+    public class Challan
+    {
+        [Required]
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+
+
+    }
+}
